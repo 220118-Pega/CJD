78b84cc1ac21947fc591b80ac0f5dec76a895a2e
CD-01-prog <86325573+CD-01-prog@users.noreply.github.com>
2022-02-03 15:46:48 -0700

    unsure what to test so commeted it out

--- P0-Expense-Report/Expense-Report/src/TransactionTest.java
+++ P0-Expense-Report/Expense-Report/src/TransactionTest.java
@@ -1,20 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*package test.java;
 
+import  org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import com.revature.Expense.models.transaction.status;
+import com.revature.Expense.models.transaction.transactiontype;
+
 /**
- * 
- */
+ * to test transaction class
+ 
 
 /**
  * @author 16del
  *
- */
+ 
 class TransactionTest {
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
-
 }
+*/
